cf7ccfd1bf6ed871e445dd2bdabab04bb5d07b14
MSSohan <sohan.cu.eee.17@gmail.com>
2023-08-22 01:17:23 +0600

    Update JARVIS.py

--- JARVIS.py
+++ JARVIS.py
@@ -1128,7 +1128,7 @@ class Main(QMainWindow):
     def showTime(self):
         current_time = QTime.currentTime()
         current_date = QDate.currentDate()
-        label_time = current_time.toString('ss:mm:hh')
+        label_time = current_time.toString('hh:mm:ss')
         label_date = current_date.toString(Qt.ISODate)
         self.ui.textBrowser.setText(label_date)
         self.ui.textBrowser_2.setText(label_time)
